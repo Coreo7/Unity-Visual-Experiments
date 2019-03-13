1a3d7233fec8a3a69e01f6bdf4be45d73a30a435
DevinT7 <devin@ghostpepper.io>
2019-03-12 22:02:56 -0700

    Added some components, prolly better to make it from one controller with a list

---
+++ Assets/Scripts/ClockedRotate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+using DG.Tweening;
+
+public class ClockedRotate : MonoBehaviour
+{
+    public float Speed;
+    public int NoteTrigger = 50, Channel = 1, CurrrentAngle = 0;
+
+    public List<Vector3> SavedAngles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        MidiMaster.noteOnDelegate += NoteOn;
+    }
+    private void OnDisable()
+    {
+        MidiMaster.noteOnDelegate -= NoteOn;
+    }
+
+    void NoteOn(MidiChannel channel, int note, float velocity)
+    {
+        Debug.Log("RotateOn: " + channel + "," + note + "," + velocity);
+
+        if (Channel == (int)channel + 1)
+        {
+            if (NoteTrigger == note)
+            {
+                if (CurrrentAngle < SavedAngles.Count)
+                {
+                    transform.DORotate(SavedAngles[CurrrentAngle], Speed).SetEase(Ease.OutFlash);
+                    CurrrentAngle++;
+                }
+                else
+                {
+                    CurrrentAngle = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FattenUp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+using DG.Tweening;
+
+
+public class FattenUp : MonoBehaviour
+{
+    public float FatJuice = 3, speed = .2f;
+    [SerializeField]
+    public int NoteTrigger = 48, Channel = 1;
+
+    private float StartScale;
+
+
+    private void OnEnable()
+    {
+    //    MidiMaster.noteOnDelegate += NoteOn;
+        StartScale = transform.localScale.x;
+    }
+    private void OnDisable()
+    {
+        MidiMaster.noteOnDelegate -= NoteOn;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NoteOn(MidiChannel channel, int note, float velocity)
+    {
+        Debug.Log("NoteOn: " + channel + "," + note + "," + velocity);
+
+        if (Channel == (int)channel + 1)
+        {
+            if (NoteTrigger == note)
+            {
+                print("FOURTY EIGHT");
+                transform.DOScale(FatJuice * velocity, speed).SetEase(Ease.OutFlash);
+                StartCoroutine(goBack());
+            }
+        }
+    }
+
+    IEnumerator goBack()
+    {
+        yield return new WaitForSeconds(speed / 2);
+        transform.DOScale(StartScale, speed).SetEase(Ease.OutFlash);
+    }
+}
