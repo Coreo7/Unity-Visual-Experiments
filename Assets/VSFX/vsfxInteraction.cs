04d5fd22a001114a037608c5c41723728b8cfb85
DevinT7 <devin@ghostpepper.io>
2019-03-15 19:55:41 -0700

    Got vsfx reacting to code

---
+++ Assets/VSFX/vsfxInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class vsfxInteraction : MonoBehaviour
+{
+    public float SpawnAmount = 9999;
+
+    public VisualEffect effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            effect.SetFloat("_myFloat", SpawnAmount);
+        }
+    }
+}
